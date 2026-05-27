093748091681083a34037e2fadadecbbae8827a6
Sonu Singh <160756068+sonu2k1@users.noreply.github.com>
2026-05-28 01:02:04 +0530

    Configure 0O3A2503.jpg in Mission and copy it to Groups directory to prevent broken links

--- src/sections/Mission/Mission.tsx
+++ src/sections/Mission/Mission.tsx
@@ -242,7 +242,7 @@ export const Mission: React.FC = () => {
               }}
             >
               <Image
-                src="/images/Groups/0O3A2503.jpg"
+                src="/images/0O3A2503.jpg"
                 alt="Beyond The View team"
                 fill
                 sizes="(max-width: 768px) 100vw, 420px"
